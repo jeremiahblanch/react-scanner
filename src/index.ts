fc0910daad71d1531ba6a624d4bde8ae8c2de02c
Jeremiah Blanch <jblanch@nrl.com.au>
2024-04-16 14:16:38 +1000

    delete unused components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import BarcodeScanner from "./barcode-scanner.tsx";
 import Scanner from "./scanner.tsx";
-import DropZone from "./drop-zone.tsx";
 import { useDecoder } from "./utils/use-decoder.ts";
 
-export { BarcodeScanner, Scanner, DropZone, useDecoder }
+export { Scanner,  useDecoder }
