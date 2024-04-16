722c78ace982c1bd2bd1b6a5de42f036151a2f1b
Jeremiah Blanch <jblanch@nrl.com.au>
2024-04-16 14:14:57 +1000

    improve vite demo

--- vite-demo/index.tsx
+++ vite-demo/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Scanner } from '../src/';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Scanner
-      style={{ width: "100vw", height: "100vh" }}
-      onScan={console.log}
-      aspectRatio="unset"
-    />
-  </React.StrictMode>,
-  document.querySelector("#root")
+const container = document.querySelector("#root");
+const root = createRoot(container!);
+root.render(
+  <Scanner
+    style={{ width: "100vw", height: "100vh" }}
+    onScan={console.log}
+    aspectRatio="unset"
+  />
 );
